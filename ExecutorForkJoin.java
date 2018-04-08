5ff6c1b8ba9def5e14f0f88703757f191ccc51a2
Unknown <lchooman@gmail.com>
2018-04-08 20:35:27 +0400

    Add GPL v3 license

--- ExecutorForkJoin.java
+++ ExecutorForkJoin.java
@@ -13,6 +13,24 @@ import java.time.Instant;
 import java.time.Duration;
 import java.util.Random;
 
+/*
+    A program to process a list in parallel using Java 8 ForkJoinPool.
+    Copyright (C) 2018 Lewis Tat Fong Choo Man
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see http://www.gnu.org/licenses/gpl.html.
+ */
+
 public class ExecutorForkJoin extends RecursiveTask<Set<Long>> {
 
 	private static final int THREADS = 4;
--- ExecutorIterators.java
+++ ExecutorIterators.java
@@ -14,6 +14,24 @@ import java.time.Duration;
 import java.util.Random;
 import java.util.function.Function;
 
+/*
+    A program to process an iterator using multiple threads in a thread safe manner.
+    Copyright (C) 2018 Lewis Tat Fong Choo Man
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see http://www.gnu.org/licenses/gpl.html.
+ */
+
 public class ExecutorIterators {
 
 	private static final int THREADS = 4;
